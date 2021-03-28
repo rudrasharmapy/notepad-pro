222c5a9b01b945f32b837e383b34be6b2f384636
rudrasharmapy <81527549+rudrasharmapy@users.noreply.github.com>
2021-03-28 23:03:08 +0530

    Added Notepad
    This is the official file.

---
+++ notepad.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+
+compiler = Tk()
+compiler.title('Meta Text')
+
+filepath=''
+
+def openfile():
+	path = askopenfilename()
+	with open(path, 'r') as file:
+		code = file.read()
+		editor.delete('1.0', END)
+		editor.insert('1.0', code)
+		setfilepath(path)
+
+def setfilepath(path):
+	global filepath
+	filepath = path
+
+def saveas():
+	if filepath == '':
+		path = asksaveasfilename()
+	else:
+		path=filepath
+	with open(path, 'w') as file:
+		code = editor.get('1.0', END)
+		file.write(code)
+		setfilepath(path)
+
+menubar = Menu(compiler)
+filename = Menu(menubar, tearoff=0)
+filename.add_command(label='Open', command=openfile)
+filename.add_command(label='Save', command=saveas)
+filename.add_command(label='Save As', command=saveas)
+filename.add_command(label='Exit', command=exit)
+menubar.add_cascade(label='File', menu=filename)
+compiler.config(menu=menubar)
+
+editor=Text()
+editor.pack()
+compiler.mainloop()
