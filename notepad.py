22f4fc2f53afb8ab99cca029cdd71715d5b8d363
rudrasharmapy <81527549+rudrasharmapy@users.noreply.github.com>
2021-03-29 22:21:01 +0530

    Ask Before Closing
    Added a simple feature. Now every time you try to close this it will ask you before closing that whether you saved your project or not.

--- notepad.py
+++ notepad.py
@@ -6,6 +6,34 @@ compiler.title('Notepad Pro')
 
 filepath=''
 
+def yessir():
+	compiler.destroy()
+
+def nosir():
+	root.destroy()
+
+def on_closing():
+
+	global root
+
+	root = Toplevel(compiler)
+	root.geometry('250x150')
+	root.config()
+
+	poplabel=Label(root, text='Are you sure you have saved your file and want to exit?')
+	poplabel.pack(pady=10)
+
+	myframe = Frame(root)
+	myframe.pack(pady=5)
+
+	yes = Button(myframe, text='Yes', command=yessir)
+	yes.pack()
+
+	no = Button(myframe, text='No', command=nosir)
+	no.pack()
+
+	root.mainloop()
+
 def openfile():
 	path = askopenfilename()
 	with open(path, 'r') as file:
@@ -33,10 +61,12 @@ filename = Menu(menubar, tearoff=0)
 filename.add_command(label='Open', command=openfile)
 filename.add_command(label='Save', command=saveas)
 filename.add_command(label='Save As', command=saveas)
-filename.add_command(label='Exit', command=exit)
+filename.add_command(label='Exit', command=on_closing)
 menubar.add_cascade(label='File', menu=filename)
 compiler.config(menu=menubar)
 
 editor=Text()
 editor.pack()
+
+compiler.protocol('WM_DELETE_WINDOW', on_closing)
 compiler.mainloop()
